bf1ea933ecfa84342e0b27f69b26042ed2deb255
Zlobryak <clychnik@gmail.com>
2025-05-25 22:58:42 +0300

    Added: Итого (руб):

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int income = 44000;
         int spending = 30000;
-
+        System.out.println("Итого (руб):");
         System.out.println(income - spending);
 
     }
