a11724624ec2a9f65ac6975d98489ce0e9006df6
Zlobryak <clychnik@gmail.com>
2025-05-25 18:21:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int income = 100000;
+        int spending = 5000;
+
+        System.out.println(income - spending);
+
+    }
+}
+
+
